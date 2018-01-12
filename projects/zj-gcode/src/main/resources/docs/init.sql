f75fecfefb691c13b98328870fc8f4d7bc93e5d8
yangxi <xi.yang@downjoy.com>
2018-01-12 14:30:35 +0800

    push

--- projects/zj-gcode/src/main/resources/docs/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `REBATE_BLACK_MID` (
-    `ID` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `MID` bigint NOT NULL COMMENT 'mid',
-    `CONTENT` varchar ( 255 ) NOT NULL COMMENT '备注',
-    `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
-    `OPERATOR` varchar ( 255 ) NOT NULL COMMENT '操作人'
-  ,  PRIMARY KEY (`ID`)
-);
-
